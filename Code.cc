9fecb718d822b5146efc471854fb3883c7697d96
lifeasawalrus <91388497+lifeasawalrus@users.noreply.github.com>
2021-09-25 11:32:48 -0700

    Update Code.cc

--- Code.cc
+++ Code.cc
@@ -1,1 +1,47 @@
+#include <iostream>
+#include "/public/colors.h"
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+
+// GAME START: You recieve a call from Star Command detailing a mission to deliver a valuable package to SC base in a nearby star system.
+//
+//
+// Before boarding the spacecraft, the place must enter their "userName" and must solve a puzzle in order to 'remember' your password (1st puzzle).
+//
+//
+// After the first failed attempt, the Player is greeted by their first mate (Commander Pepe) who drops a subtle hint that contains the answer to this puzzle.
+//
+//
+// Once onboard, the player is introduced to their crew and some of the commands they can enter to navigate the game and names their brand new "string starShip" before embarking on it's maiden voyage.
+//      These commands include how to enter "void navigation()" mode where a map of the star system comes onto screen and the player may move the starship to another location.
+//      Responding to an incoming "HAIL" is introduced when the Star Command general hails the player's starShip to wish them good luck on the mission.
+//      Commander Pepe advises the player that he can consult him at anytime with command "ADVICE" to get a hint about what they should do next.
+//        
+//
+// Upon leaving earth, after the player moves "X" spaces on the worldmap, the screen flashes "void damage()": Commander Pepe advises the player that they will need to "void fixEngine()" (2nd puzzle) in order to visit the Star Command base on the Jupiter moon "TITAN" where there the only "bool warpDrive" manufacturing company exists in this star system.
+//
+//
+// After moving to "ENGINEERING" and aiding the chief of engineering in fixing the engines, the player may now "void navigation()" and navigate to "TITAN".
+//
+//
+// Upon arrival the player must 'remember' their Star Command interstellar ID to safely land on "TITAN" (3rd puzzle)
+// 
+// After landing on at the base, the player meets Bezos, the dismembered head in a jar that owns "TITAN" and the warp-drive manufacturing company.
+//
+//
+// The player must convince Bezos that the mission is dire and the aquisition of a warpDrive is critical for the future of Star Command (4th puzzle).
+//
+//
+// The player returns to the starShip and must return to "ENGINEERING" and use the "warDrive" from their inventory to repair the propulsion system (5th puzzle).
+//
+//
+//
+
+
+
+}
