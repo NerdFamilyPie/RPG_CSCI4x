6d7631f449ef05bdc8434eaa0a74152e2460d757
lifeasawalrus <91388497+lifeasawalrus@users.noreply.github.com>
2021-09-27 17:44:34 -0700

    Add files via upload
    This is pretty flushed out start. I included the SPACE(); map and revised the dialogue. I also fixed the WRDSRCH(); engine to work from vectors that are initialized above that function. The place for you to insert the password dialogue/puzzle is commented out. I will branch this branch when I finish the dialogue to assess damages before the function call to fix the engine!

--- main.cc
+++ main.cc
@@ -5,6 +5,223 @@
 #include <algorithm>
 using namespace std;
 
+// MAP FUNCTION**************************************************************************************************
+// This will be a function that returns a char, which will determine what occurs in the next part according to the location of the player
+vector<string> world_map = {
+        "________________________",
+        "|               .      |",
+        "|   .          .   .   |",
+        "|.    .   .            |",
+        "| .               .    |",
+        "|          .          .|",
+        "|             .        |",
+        "| .       .            |",
+        "|                      |",
+        "|   . .        .  .    |",
+        "|     .         .      |",
+        "|.       .      .      |",
+        "|                 .  . |",
+        "^^^^^^^^^^^^^^^^^^^^^^^^",
+};
+
+void damage() {
+        for (int j = 0; j < 9; j++) {
+                setbgcolor(255,0,0);
+                for (int i = 0; i < 100'000; i++)
+                        cout << ' ';
+                setbgcolor(255,255,255);
+                for (int i = 0; i < 100'000; i++)
+                        cout << " \a";
+                }
+        setbgcolor(0,0,0);
+        clearscreen();
+        cout << endl;
+        system("clear");
+}
+
+
+char get_world_location(size_t row, size_t col) {
+        if (row >= world_map.size()) return ' ';
+        if (col >= world_map.at(row).size()) return ' ';
+        return world_map.at(row).at(col);
+}
+
+void print_world(size_t player_row, size_t player_col, size_t titan_row, size_t titan_col, size_t Jrow, size_t Jcol, size_t Arow, size_t Acol, size_t Erow, size_t Ecol) {
+        clearscreen();
+        movecursor(0,0);
+        for (size_t row = 0; row < world_map.size(); row++) {
+                for (size_t col = 0; col < world_map.at(row).size(); col++) {
+                        if (row == player_row and col == player_col) cout << RED << '%' << RESET;
+                        else if (row == Jrow and col == Jcol) {
+                                if (Jrow != player_row or Jcol != player_col) {
+                                setcolor(255,165,0);
+                                cout << 'J' << RESET;
+                                }
+                        }
+                        else if (row == Erow and col == Ecol) {
+                                if (Erow != player_row or Ecol != player_col) cout << BLUE << 'E' << RESET;
+                        }
+                        else if (row == Arow and col == Acol) {
+                                if (Arow != player_row or Acol != player_col) {
+                                        setcolor(255,255,0);
+                                        cout << 'A' << RESET;
+                                }
+                        }
+                        else if (row == titan_row and col == titan_col) {
+                                setcolor(102,153,204);
+                                cout << 'T' << RESET;
+                        }
+                        else
+                                cout << world_map.at(row).at(col);
+                }
+                cout << endl;
+        }
+}
+
+const size_t tr1 = 4, tr2 = 5, tr3 = 6, tc1 = 4, tc2 = 5, tc3 = 6, tc4 = 7, tc5 = 8;
+
+int move_titan(size_t &titan_row, size_t &titan_col) {
+        if (titan_row == tr1) {
+                if (titan_col == tc2) {titan_col = tc3;}
+                else if (titan_col == tc3) {titan_col = tc4;}
+                else if (titan_col == tc4) {titan_col = tc5, titan_row = tr2; return 0;}
+        }
+        if (titan_row == tr2) {
+                if (titan_col == tc5) {titan_col = tc4, titan_row = tr3;}
+                else if (titan_col == tc1) {titan_col = tc2, titan_row = tr1; return 0;}
+        }
+        if (titan_row == tr3) {
+                if (titan_col == tc4) {titan_col = tc3;}
+                else if (titan_col == tc3) {titan_col = tc2;}
+                else if (titan_col == tc2) {titan_col = tc1, titan_row = tr2;}
+        }
+        return 0;
+}
+
+
+
+char SPACE(char &locale, bool &warp_D_online, bool &first_flight, int &crash_row, int &crash_col) {
+
+        int row, col, inputCounter = 0;
+        size_t titan_row = tr1, titan_col = tc4;
+        const size_t Jrow = 5, Jcol = 6, Arow = 1, Acol = 22, Erow = 11, Ecol = 4;
+        const int ROWS = world_map.size();
+        const int COLS = world_map.at(0).size();
+        const int FPS = 5;
+        if (locale == 'E') row = 11, col = 5;
+        if (locale == 'T') row = 4, col = 3;
+        if (locale == 'C') row = crash_row, col = crash_col;
+        set_raw_mode(true);
+        show_cursor(false);
+        while (true) {
+                int c = toupper(quick_read());
+                if (c == 'W' or c == UP_ARROW) {
+                        inputCounter++;
+                        move_titan(titan_row, titan_col);
+                        if (row == titan_row and col == titan_col) {
+                                set_raw_mode(false);
+                                show_cursor(true);
+                                movecursor(0,0);
+                                clearscreen();
+                                locale = 'T';
+                                return 'T';
+                        }
+                        row--;
+                        if (row < 1) row = world_map.size() - 2;
+                }
+                if (c == 'S' or c == DOWN_ARROW) {
+                        inputCounter++;
+                        move_titan(titan_row, titan_col);
+                        if (row == titan_row and col == titan_col) {
+                                set_raw_mode(false);
+                                show_cursor(true);
+                                movecursor(0,0);
+                                clearscreen();
+                                locale = 'T';
+                                return 'T';
+                        }
+                        row++;
+                        if (row >= world_map.size() - 1) row = 1;
+                }
+                if (c == 'A' or c == LEFT_ARROW) {
+                        inputCounter++;
+                        move_titan(titan_row, titan_col);
+                        if (row == titan_row and col == titan_col) {
+                                set_raw_mode(false);
+                                show_cursor(true);
+                                movecursor(0,0);
+                                clearscreen();
+                                locale = 'T';
+                                return 'T';
+                        }
+                        col--;
+                        if (col < 1) col = world_map.at(row).size() - 2;
+                }
+                if (c == 'D' or c == RIGHT_ARROW) {
+                        inputCounter++;
+                        move_titan(titan_row, titan_col);
+                        if (row == titan_row and col == titan_col) {
+                                set_raw_mode(false);
+                                show_cursor(true);
+                                movecursor(0,0);
+                                clearscreen();
+                                locale = 'T';
+                                return 'T';
+                        }
+                        col++;
+                        if (col > world_map.at(row).size() - 2) col = 1;
+                }
+                print_world(row, col, titan_row, titan_col, Jrow, Jcol, Arow, Acol, Erow, Ecol);
+                //move_titan(titan_row, titan_col); //makes constant orbit while stationary
+                movecursor(0,0);
+                cout.flush();
+                if (first_flight == 1) {
+                        if (inputCounter == 7) {
+                                damage();
+                                set_raw_mode(false);
+                                show_cursor(true);
+                                movecursor(0,0);
+                                clearscreen();
+                                crash_row = row, crash_col = col;
+                                first_flight = 0;
+				warp_D_online = 0;
+                                locale = 'C';
+                                return 'C';
+                        }
+                }
+                if (row == Erow and col == Ecol and !first_flight) {
+                        set_raw_mode(false);
+                        show_cursor(true);
+                        movecursor(0,0);
+                        clearscreen();
+                        locale = 'E';
+                        return 'E';
+                }
+                if (row == titan_row and col == titan_col) {
+                        set_raw_mode(false);
+                        show_cursor(true);
+                        movecursor(0,0);
+                        clearscreen();
+                        locale = 'T';
+                        return 'T';
+                }
+                if (row == Arow and col == Acol and warp_D_online == 1) {
+                        set_raw_mode(false);
+                        show_cursor(true);
+                        movecursor(0,0);
+                        clearscreen();
+                        locale = 'A';
+                        return 'A';
+                }
+                if (c == ERR) usleep(1'000'000 / FPS);
+        }
+}
+// MAP FUNCTION**************************************************************************************************
+
+
+
+
+
 // Modifies type speed for conversational effect.
 void TYPER(string GM) {
         for (int i = 0; i < GM.size(); i++) {
@@ -38,7 +255,7 @@ void TYPERFAST(string GM) {
 
 // Inputs pause for dramatic effect.
 void NarrP() {
-        usleep (1'000'000);  /*1'500'000*/
+        usleep (1'500'000);  /*1'500'000*/
 }
 
 
@@ -53,37 +270,29 @@ void elipse() {
 
 
 // Changes input to all caps.
-string TOUPPER(string userString) {
+string TOUPPER(string &userString) {
         for (size_t i = 0; i < userString.size(); i++) {
         userString.at(i) = toupper(userString.at(i));
         }
         return userString;
 }
 
+vector<string> key_word_answer {"ANSWER","PICK UP","YES","ON SCREEN"};
+vector<string> key_word_ignore {"IGNORE","NO","DONT","DON'T","COFFEE"};
+vector<string> key_word_yes {"YES","SURE","OKAY","DEFINITELY"};
+vector<string> key_word_no {"NO","DONT","DON'T","NOPE","NAH"};
+vector<string> key_word_launch {"LAUNCH","TAKE OFF","FLY","GO","NAVIGATE","NAVIGATION"};
+vector<string> key_word_advice {"HELP","ADVICE","IDEA"};
+
 
 // Searches user input for a keyword. FIX ME: needs to be changed to call a vector<string> &keyWord for optimization.
-bool WRDSRCH(string userString, string keyPhrase) {
-        if (userString.find(keyPhrase) != string::npos) {
-                return 1;
-        }
-        else {
-                return 0;
-        }
-}
-
-void damage() {
-        for (int j = 0; j < 9; j++) {
-                setbgcolor(255,0,0);
-                for (int i = 0; i < 100'000; i++)
-                        cout << ' ';
-                setbgcolor(255,255,255);
-                for (int i = 0; i < 100'000; i++)
-                        cout << " \a";
-                }
-        setbgcolor(0,0,0);
-        clearscreen();
-        cout << endl;
-        system("clear");
+bool WRDSRCH(string userString, vector<string> keyPhrase) {
+	for (size_t i = 0; i < keyPhrase.size(); i++) {
+		if (userString.find(keyPhrase.at(i)) != string::npos) {
+			return 1;
+		}
+	}
+	return 0;
 }
 
 
@@ -115,42 +324,58 @@ int main() {
 
 	for(int i = 0; i < 3; i++) cout << endl;
 	elipse();
-	TYPER("It's approximately 0600 hours\nStar Date: 46436.3014"); NarrP();
-	TYPER("The pinnacle of coffee scents fills your nostrils.");
+	TYPER("It's approximately 0600 hours."); NarrP(); TYPR("Star Date: 46436.3014"); elipse(); cout << endl; cout << endl;
+	TYPER("The pinnacle of coffee scents fills your nostrils."); NarrP();
 	TYPR("This wasn't prepared in a replicator"); elipse(); cout << endl;
-       	TYPER("You are you sitting on the rooftop balcony of the");
+       	TYPER("You are sitting on the rooftop balcony of the");
 	TYPER("best coffee shop in the Federation."); NarrP(); cout << endl;
-	TYPER("It sits just on the outskirts of New York City.");
+	TYPER("It sits just on the outskirts of New York City."); NarrP();
 	TYPER("Based on the weather patterns in the sky,");
 	TYPER("you sense that this will be one of the most ");
-	TYPR("beautiful sunrises over the city that you've ever seen\n"); elipse();
+	TYPR("beautiful sunrises over the city that you've ever seen.\n"); NarrP(); 
+	TYPER("What a great day to have off!"); elipse();
 	while (true) {
 		elipse(); cout << endl;
-		TYPER("Someone is hailing you on your communicator.\a");
+		TYPER("Someone is hailing you on your communicator!\a");
 		TYPER("What would you like to do?");
 		getline(cin, action);
-		if (action == "ignore") TYPER("You ignore the call and take another glorius sip."); 
-		else if (action == "answer") break;
-		else continue;
+		TOUPPER(action);
+		if (WRDSRCH(action, key_word_ignore)) TYPER("You ignore the call and take another glorius sip.");
+		else if (WRDSRCH(action, key_word_answer)) break;
+		else TYPER("You can't do that now.");
 	}
-	TYPER("It's General Pepper from Star Command.");
-	TYPR("GENERAL: "); NarrP(); TYPER("Captain, the Federation is in dire need of your assistance."); NarrP();
-	TYPER("We need you to deliver a valuable care-package on a top secret mission to Andromeda."); NarrP();
+	TYPR("You answer the call"); elipse(); cout << endl;
+	TYPER("It's General Pepper from Star Command!"); NarrP();
+	TYPR("GENERAL: "); NarrP(); TYPER("Captain, the Federation is in dire need of your assistance!"); NarrP();
 	TYPER("We have already assembled a crew and readied a new space craft."); NarrP();
 	TYPER("Please be careful with this one, as there was nothing ");
-	TYPER("salvageable from your last *ahem* accident.");
+	TYPR("salvageable from your last "); usleep(250'000); TYPR("*ahem*"); usleep(500'000); TYPER(" accident.");
 	elipse();
 	TYPER("Will you accept the mission?");
 	getline(cin, action);
-	TYPR("GENERAL: "); elipse(); TYPER("Thanks! I knew you would do it!"); NarrP();
+	TOUPPER(action);
+		if (WRDSRCH(action, key_word_yes)) {
+			TYPR("GENERAL: "); elipse(); TYPER("Thanks! I can always count on you!"); NarrP();
+			}
+		else {
+			while (true) {
+				TYPR("GENERAL: "); NarrP(); TYPER("Sorry, I didn't get that"); elipse(); TYPER("What?");
+				getline(cin, action);
+				TOUPPER(action);
+				if (WRDSRCH(action, key_word_yes)) {
+					TYPR("GENERAL: "); elipse(); TYPR("Great!"); TYPR(" I always know I can count on you!"); NarrP(); TYPER(" ;O"); NarrP();
+					break;
+				}
+			}
+		}
 	TYPER("Head to terminal 96 at the space port."); NarrP();
-	TYPER("I'll brief you once board."); NarrP();
-	TYPER("\a*click*");
+	TYPER("I'll brief you once on board."); NarrP();
+	TYPER("*click*\a");
 	elipse(); cout << endl;
-	TYPR("Your coffee is cold"); elipse(); TYPER("Guess it's time to go!");
-       
+	TYPR("Your coffee is cold"); elipse(); TYPER("Guess it's time to go!"); 
 	elipse(); elipse(); cout << endl;
 	
+
 	// REPLACE WITH puzzle1();
 	//
 	/*// Puzzle1
@@ -160,8 +385,83 @@ int main() {
 	TYPER("It prompts you for your password");
 	// Enter password loop.*/
 
+	
+	// First Time aboard the Starship.
+	int crash_row = 0, crash_col = 0;
+	bool first_flight = 1; //this will be one at game start, then 0 for the rest of the game
+	bool warp_D_online = 1;
+	string location_ship = "Command Bay";
+	char locale = 'E';
+	TYPER("You board the Starship!"); NarrP();
+	TYPR("COMMANDER: "); NarrP(); TYPER("Welcome aboard sir!"); NarrP();
+	TYPER("If you need help at any time, you can ask me for advice!");
+	bool quit = 0;
+	while (quit == 0) {
+		TYPER("What would you like to do?");
+		getline(cin, action);
+		TOUPPER(action);
+		if (WRDSRCH(action, key_word_advice)) {
+			// HELP1();
+			TYPR("We are currently in "); TYPR(location_ship); TYPER("."); NarrP();
+			TYPER("If the ship gets hailed, you can give the order ");
+			TYPER("\"on screen.\""); NarrP();
+			TYPER("We need to begin the mission by taking off.");
+		}
+		else if (WRDSRCH(action, key_word_launch)) {
+			while(true) {
+				elipse(); cout << endl; TYPR("COMMS: "); NarrP(); TYPER("Incoming transmission from Star Command!");
+				TYPER("What would you like to do?");
+				getline(cin, action);
+				TOUPPER(action);
+				if (WRDSRCH(action, key_word_answer)) {
+					TYPER("It's General Pepper!"); NarrP();
+					TYPR("GENERAL: "); NarrP(); TYPER("Morning Captain! This mission will be pretty routine.");
+					TYPER("We just need you to deliver a package to our base in Andromeda."); NarrP();
+					TYPER("Do be sure to navigate your ship to the edge of the solar system ");
+					TYPER("before jumping past warp-1."); NarrP();
+					TYPER("We're experiencing an abnormal amount of debris in the system."); NarrP();
+					TYPER("Good luck!");
+					elipse(); TYPER("*click*\a");
+					NarrP(); cout << endl; TYPER("The ship's engines are warmed and everything is in order."); NarrP();
+					TYPER("The ship leaves the dock and begins it's maiden voyage!");
+					elipse(); TYPR("COMMANDER: "); TYPER("Hey Cap, "); NarrP(); TYPER("Once we get into space, you ought to take the helm and show ");
+					TYPER("us some of those piloting skills you're famous for!");
+					TYPR("Pilot: "); NarrP(); TYPER("Yes Captain! Please take the helm!"); NarrP();
+					elipse(); cout << endl;
+					TYPER("You decide to take the helm."); NarrP(); TYPER("Everyone seems really excited!");
+					elipse();
+					usleep(2'000'000);
+					quit = 1;
+					break;
+				}
+				else if (WRDSRCH(action, key_word_ignore)) {
+					NarrP(); cout << endl; TYPER("The ship's engines are warmed and everything is in order."); NarrP();
+					TYPER("The ship leaves the dock and begins it's maiden voyage!");
+					elipse(); cout << endl; TYPR("COMMANDER: "); TYPR("Hey Cap, "); NarrP(); TYPER("Once we get into space, you ought to take the helm and show ");
+					TYPER("us some of those piloting skills you're famous for!");
+					TYPR("Pilot: "); NarrP(); TYPER("Yes Captain! I would be honored to offer you the helm"); NarrP();
+					elipse(); cout << endl;
+					TYPER("You decide to take the helm."); NarrP(); TYPER("Everyone seems really excited!");
+					elipse();
+					usleep(3'000'000);
+					quit = 1;
+					break;
+				}
+				else TYPER("You can't do that now!");
+			}
+		}
+		else TYPER("You can't do that now!");
+	}
+	SPACE(locale, warp_D_online, first_flight, crash_row, crash_col);
 
 
+	// After crash
+	TYPER("YOU HIT AN ASTEROID AND DAMAGED THE SHIP!!!"); 
+	NarrP(); cout << endl;
+}
+
+
+	
 
 // GAME START: You recieve a call from Star Command detailing a mission to deliver a valuable package to SC base in a nearby star system.
 //
@@ -204,6 +504,3 @@ int main() {
 // "void battle()" is called and the player engages in battle. They may get "ADVICE" from Commander Pepe who tells them to use proton torpedos sparingly, and save them for when the enemy shields are down.
 // 	If player ship takes damage "void damage()" is called. If "playerHP == 0" they restart from the drop out of warp speed. If "enemyHP == 0" they land sucesfully on the planet and deliver the cargo. Player wins!
 
-
-
-}
